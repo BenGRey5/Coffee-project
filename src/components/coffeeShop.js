1468ac66b10b6b7f61661cee9ccf01e20a271156
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-12-18 06:42:58 -0800

    Update coffeeShop.js

--- src/components/coffeeShop.js
+++ src/components/coffeeShop.js
@@ -31,44 +31,4 @@ export default Item;
 
 
 
-// import React from 'react';
-// import PropTypes from 'prop-types';
-
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import Item from './coffeeShop.js';
-
-// function ItemList({ items, onSell, onDelete, onUpdate, }) {
-//     return (
-//         <div>
-//             {items.map(item => (
-//                 <Item
-//                     key={item.id}
-//                     item={item}
-//                     onSell={() => onSell(item.id)}
-//                     onDelete={() => onDelete(item.id)}
-//                     onUpdate={(updatedItem) => onUpdate(item.id, updatedItem)}
-//                 />
-//             ))}
-//         </div>
-//     );
-// }
-
-// ItemList.propTypes = {
-//     items: PropTypes.arrayOf(
-//         PropTypes.shape({
-//             id: PropTypes.number.isRequired,
-//             name: PropTypes.string.isRequired,
-//             description: PropTypes.string.isRequired,
-//             quantity: PropTypes.number.isRequired,
-//         })
-//     ).isRequired,
-//     onSell: PropTypes.func.isRequired,
-//     onDelete: PropTypes.func.isRequired,
-//     onUpdate: PropTypes.func.isRequired,
-// };
-
-// export default ItemList;
-
-
 
