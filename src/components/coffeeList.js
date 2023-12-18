a0d1e15f5492f8b48a9822b56cb05e1b447c38a0
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-12-18 06:43:30 -0800

    Update coffeeList.js

--- src/components/coffeeList.js
+++ src/components/coffeeList.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import Item from './coffeeShop.js'
+import PropTypes from 'prop-types';
+import Item from './coffeeShop.js';
 
-function ItemList({ items, onSell, onDelete, onUpdate, onDescribe }) {
+function ItemList({ items, onSell, onDelete, onUpdate, toggleDescription }) {
     return (
-        <div className="App">
-            <h1>The Mean Bean</h1>
-            <OptionList onAddItem={addItem} />
-            <ItemList
-                items={items}
-                onSell={sellItem}
-                onDelete={deleteItem}
-                onUpdate={updateItem}
-                onDescribe={toggleDescription} // Pass the toggleDescription function to the ItemList component
-            />
-            {form2visible && (
-                <div>
-                    <button onClick={() => toggleForm("form2")}>Return</button>
-                    <h2>{selectedItem}</h2>
-                    <form>
-                        {/* Your radio buttons and other form elements */}
-                    </form>
-                    <p>{selectedDescription}</p>
-                </div>
-            )}
+        <div>
+            {items.map(item => (
+                <Item
+                    key={item.id}
+                    item={item}
+                    onSell={() => onSell(item.id)}
+                    onDelete={() => onDelete(item.id)}
+                    onUpdate={(updatedItem) => onUpdate(item.id, updatedItem)}
+                    
+                />
+            ))}
         </div>
     );
 }
+
+ItemList.propTypes = {
+    items: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            name: PropTypes.string.isRequired,
+            description: PropTypes.string.isRequired,
+            quantity: PropTypes.number.isRequired,
+        })
+    ).isRequired,
+    onSell: PropTypes.func.isRequired,
+    onDelete: PropTypes.func.isRequired,
+    onUpdate: PropTypes.func.isRequired,
+
+};
 
 export default ItemList;
